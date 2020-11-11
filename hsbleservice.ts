b1f93af29cc40d56b3e7dcc54034e1d7357920d2
stem-hanshin <59435476+stem-hanshin@users.noreply.github.com>
2020-11-11 17:52:08 +0800

    更新 pxt.json, hsbleservice.ts, hsbleservice.cpp

---
+++ hsbleservice.ts
@@ -0,0 +1,35 @@
+ /**
+ * Bluetooth BLE Service
+ */
+//% color=#0082FB weight=96 icon="\uf294" block="HANSHIN: Bluetooth BLE Service"
+namespace hsbleservice
+{
+	export let NEW_LINE = "\r\n";
+    /**
+    *  Writes to the Bluetooth UART service buffer. From there the data is transmitted over Bluetooth to a connected device.
+    */
+    //% blockId=bleWriteString block="ble write string %data" blockGap=8
+    //% parts="hsbleservice" shim=hsbleservice::bleWriteString
+    export function bleWriteString(data: string): void {
+        console.log(data)
+    }
+
+    /**
+    *  Writes to the Bluetooth UART service buffer. From there the data is transmitted over Bluetooth to a connected device.
+    */
+    //% blockId=bleWriteLine block="ble write line %data" blockGap=8
+    //% parts="hsbleservice"
+    export function bleWriteLine(data: string): void {
+        bleWriteString(data + serial.NEW_LINE);
+    }
+	
+	/**
+     *  Reads from the Bluetooth UART service buffer, returning its contents when the specified delimiter character is encountered.
+     */
+    //% blockId=bleReadUntil block="ble read until %del=serial_delimiter_conv"
+    //% parts="hsbleservice" shim=hsbleservice::bleReadUntil
+    export function bleReadUntil(del: string): string {
+        // dummy implementation for simulator
+        return ""
+    }
+}
